34fbcfa9dbafedb23eeb1cc8126d33f477c8b77b
exionite <exionite@gmail.com>
2022-08-31 09:58:18 -0700

    added circles and icons to their centers

--- src/components/PlayField.js
+++ src/components/PlayField.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import style from './Styles.module.scss'
-// import triangle from "/images/bg-triangle.svg"
+import style from './Styles.module.css'
 
 const PlayField = () => {
   return (
     <div className={style.playfield}>
       <img src={process.env.PUBLIC_URL + "/images/bg-triangle.svg"} alt="" />
+      <div className={style.rockContainer}>
+        <span className={style.circle}>
+          <img src={process.env.PUBLIC_URL + "/images/icon-rock.svg"} alt="" />
+        </span>
+      </div>
+      <div className={style.paperContainer}>
+        <span className={style.circle}>
+          <img src={process.env.PUBLIC_URL + "/images/icon-paper.svg"} alt="" />
+        </span>
+      </div>
+      <div className={style.scissorContainer}>
+        <span className={style.circle}>
+          <img src={process.env.PUBLIC_URL + "/images/icon-scissors.svg"} alt="" />
+        </span>
+      </div>
     </div>
   );
 }
