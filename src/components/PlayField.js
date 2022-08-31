6306ffb5a7cf16f5c7cfb09ace4169fc4d459153
exionite <exionite@gmail.com>
2022-08-31 08:42:49 -0700

    triangle image added, changed styles.module.css to module.scss

--- src/components/PlayField.js
+++ src/components/PlayField.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import style from './Styles.module.css'
+import style from './Styles.module.scss'
+// import triangle from "/images/bg-triangle.svg"
 
 const PlayField = () => {
   return (
     <div className={style.playfield}>
-      PlayField
+      <img src={process.env.PUBLIC_URL + "/images/bg-triangle.svg"} alt="" />
     </div>
   );
 }
